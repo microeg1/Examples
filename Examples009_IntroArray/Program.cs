59aacb7770b35082f4541ed09e09399987545cb4
Egor <pro100egar@gmail.com>
2022-08-20 19:45:20 +0700

    array add

---
+++ Examples009_IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
+}
+
+int[] array = {11,12,15,176,174,347,234,412,123};
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
