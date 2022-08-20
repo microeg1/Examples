7f87a14aa4461c865aadfefcd6df37c3953b8294
Egor <pro100egar@gmail.com>
2022-08-20 20:27:01 +0700

    arraylibrary add

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)            // заполняет список рандомными значениями
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);         // генерирует рандомные значения
+        index++;
+    }
+}
+
+void PrintArray(int[] col)             // печатает список
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)     //ищет число в массиве, которое соответствует запрашиваемому find
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;  //принято выводить, если элемент не найден
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;        //возвращает индекс числа
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+int pos = IndexOf(array,4);
+Console.WriteLine(pos);
